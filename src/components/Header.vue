b847c74f6a8e4e5f9241ee44945011856c95641f
cromeo98 <romeo.christian98@gmail.com>
2021-07-16 17:54:07 +0200

    basic structure

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="lead">
+    ciao
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="lead">
+      prova
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
