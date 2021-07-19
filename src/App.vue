6e12e6cc5979d74f7ed6a1d55ae25e245bb31ad1
cromeo98 <romeo.christian98@gmail.com>
2021-07-19 11:57:58 +0200

    bulding left side of navbar - icon&svg

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,9 @@
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Main from './components/Main.vue'
-import Footer from './components/Footer.vue'
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -53,7 +53,14 @@
 
         <!-- search and icons section  -->
         <div class="col-4">
-
+          <ul>
+            <li><i class="fas fa-search"></i></li>
+            <li>
+              <img id="settings-icon" src="../assets/img/settings-svg.svg"  alt="svg settings">
+            </li>
+            <li><i class="fab fa-github"></i></li>
+            <li><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M15.71 12.71a6 6 0 1 0-7.42 0a10 10 0 0 0-6.22 8.18a1 1 0 0 0 2 .22a8 8 0 0 1 15.9 0a1 1 0 0 0 1 .89h.11a1 1 0 0 0 .88-1.1a10 10 0 0 0-6.25-8.19zM12 12a4 4 0 1 1 4-4a4 4 0 0 1-4 4z" fill="#626262"/></svg></li>
+          </ul>
         </div>
 
       </div>
