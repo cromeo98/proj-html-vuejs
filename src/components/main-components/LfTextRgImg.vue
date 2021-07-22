9dfada4b3c8ec243173743859813173747454653
cromeo98 <romeo.christian98@gmail.com>
2021-07-22 11:14:54 +0200

    end jumbo sect

--- src/components/main-components/LfTextRgImg.vue
+++ src/components/main-components/LfTextRgImg.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="jumbotron">
     <div class="container">
-      <div class="row ms_red-bord">
+      <div class="row ms_red-bord align-items-center mt-5 py-5">
 
-        <aside class="col">
-          <h5>Development</h5>
-          <h2>A Complete Developer Toolset</h2>
-          <p>Launch your campaign and benefit from our expertise on designing and managing conversion centered bootstrap v5 html page.</p>
+        <aside class="col ms_blue-bord">
+          <h5 class="ms_light-btn">Development</h5>
+          <h2 class="ms_w-75x ms_title pt-1 pb-2">A Complete Developer Toolset</h2>
+          <p class="ms_paragraph ms_grey-text pb-3">Launch your campaign and benefit from our expertise on designing and managing conversion centered bootstrap v5 html page.</p>
 
           <!-- button get startd -->
-          <button>Get Started</button>
+          <button class="ms_blue-btn me-3">Get Started</button>
 
           <!-- button documentation -->
-          <button>Document</button>
+          <button class="ms_white-btn">Documentation</button>
 
-          <p>Current Version: v3.5.0</p>
+          <p class="ms_grey-text pt-3">Current Version: v3.5.0</p>
 
         </aside>
-        <aside class="col">
+        <aside class="col text-center ms_blue-bord">
           <img src="../../assets/img/about.png" alt="developer avatar developing">
         </aside>
       </div>
@@ -32,6 +32,29 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+.ms_light-btn{
+  font-size: 0.75rem;
+  border: 1px solid #d2daf5;
+  text-align: center;
+  border-radius: 15px;
+  background-color: #e3e8f8;
+  color: #2f55d4;
+  display: inline-block;
+  padding: 3px 8px;
+  font-weight: 600;
+}
+
+.ms_title{
+  font-size: 44px;
+  font-weight: 700;
+  line-height: 1.4;
+}
+
+.ms_paragraph{
+  font-size: 17.5px;
+}
 
 </style>
+
